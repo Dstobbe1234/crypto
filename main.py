b78e121d133c87f5658166a2fd8b62e553c47130
David Stobbe <d.stobbe1@share.epsb.ca>
2023-09-12 13:30:09 -0600

    get BTC value

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
-import pandas
+from requests import Session
+import matplotlib
+import time
 
-url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-parameters = {
-#   'start':'1',
-#   'limit':'5000',
-#   'convert':'USD', 
-  'symbol': 'BTC'
-  
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
-}
+def getPrice (): 
 
-session = Session()
-session.headers.update(headers)
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = { 'slug': 'bitcoin', 'convert': 'USD' } 
 
-try:
-  response = session.get(url, params=parameters, headers=headers)
-  data = json.loads(response.text)
-  print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': '5439fc0f-cfda-4c0b-a06a-96323ddd906e',
+    }
+    session = Session() 
+    session.headers.update(headers) 
+    response = session.get(url, params=parameters) 
+    price = (response.json().get("data").get("1").get("quote").get("USD").get("price"))
+    print(price)
+
+getPrice() 
+
+
